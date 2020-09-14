25dc8afc51f49602908d54f0a4437b185ff24fdc
anh.tran(c0720i1) <69283798+theanh111@users.noreply.github.com>
2020-09-14 22:13:50 +0700

    bai tap Tim gia tri nho nhat trong mang

--- src/FindMinValueInArray.java
+++ src/FindMinValueInArray.java
@@ -1,6 +1,6 @@
 public class FindMinValueInArray {
     public static void main(String[] args) {
-        int[] array = {8, 5, 7, 6, 12, 55, 1, 6, 464};
+        int[] array = {8, 5, 7, 6, -12, 55, 1, 6, 464};
         int min = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] < min) {
